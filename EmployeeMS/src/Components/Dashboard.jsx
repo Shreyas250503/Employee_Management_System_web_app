bd0ac3294b1bffbea1d49c85d7bf403ae9153f7b
Shreyas Kartikey <shreyaskartikey25@gmail.com>
2024-02-15 23:17:37 +0530

    Name change

--- EmployeeMS/src/Components/Dashboard.jsx
+++ EmployeeMS/src/Components/Dashboard.jsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
                 </div>
                 <div className="col p-0 m-0">
                     <div className="p-2 d-flex justify-content-center shadow">
-                        <h4>Emoployee Management System</h4>
+                        <h4>Emoployer Management System</h4>
                     </div>
                     <Outlet />
                 </div>
